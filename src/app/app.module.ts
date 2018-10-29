5d1f60da65df6440480672c6559e4b80e57cfd8c
EugeneRobb1 <eugenerobb@outlook.com>
2018-10-29 13:01:52 +0000

    Project refactoring - adding coming soon component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { ComingsoonComponent } from './comingsoon/comingsoon.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    ComingsoonComponent
   ],
   imports: [
     BrowserModule,
