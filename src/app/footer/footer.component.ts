62eab6e03e5e4cad8c19fd59e9f33080636cc619
EugeneRobb1 <eugenerobb@outlook.com>
2018-12-31 13:06:10 +0000

    feature: adding footer component

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  currentYear;
 
   constructor() { }
 
   ngOnInit() {
+    this.getCurrentYear()
+  }
+
+  getCurrentYear(){
+    this.currentYear = new Date().getFullYear();
   }
 
 }
